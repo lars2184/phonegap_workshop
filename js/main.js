8fcd749b221db1117c296508c8b93d2825faf3d2
lars2184 <larsjacobson@gmail.com>
2014-01-15 13:32:01 -0800

    1

--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+        
+        var self = this;
+	    this.store.findByName($('.search-key').val(), function(employees) {
+	        $('.employee-list').html(self.employeeLiTpl(employees));
+	    });
     },
 
     initialize: function() {
     	var self = this;
+    	
+    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    	
         this.store = new MemoryStore(function(){
-        	self.showAlert('Store Initialized', 'Info');
+        	//self.showAlert('Store Initialized', 'Info');
         	self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
@@ -29,8 +29,8 @@ var app = {
 	            "<input class='search-key'/>" +
 	            "<ul class='employee-list'></ul>" +
 	            "</div>";
-	    $('body').html(html);
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	    $('body').html(this.homeTpl());
+    	$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
     
     showAlert: function(message, title){
